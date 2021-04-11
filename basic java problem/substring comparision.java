4534d27c5974df1dc10e9a2b9366b60ff462fa65
Sruthi A R <78865129+SruthiGIT-cloud@users.noreply.github.com>
2021-04-11 10:27:09 +0530

    Update substring comparision.java

--- basic java problem/substring comparision.java
+++ basic java problem/substring comparision.java
@@ -8,6 +8,7 @@ public class Solution {
     String smallest = s.substring(0,k);
 for(int i=0;i<s.length()-k+1;i++){
     //*******************string array comaparision*******************
+    
 if(s.substring(i,i+k).compareTo(smallest) <0) smallest = s.substring(i,i+k);
 if(s.substring(i,i+k).compareTo(largest) >0) largest = s.substring(i,i+k); 
         }
