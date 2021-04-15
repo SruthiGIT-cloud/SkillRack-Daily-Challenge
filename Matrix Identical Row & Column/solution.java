cd2cf1e46851a95357827f0862a4abccb7c0f5fc
Sruthi A R <78865129+SruthiGIT-cloud@users.noreply.github.com>
2021-04-15 12:28:19 +0530

    Create solution.java

---
+++ Matrix Identical Row & Column/solution.java
@@ -0,0 +1,98 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int flag=0;
+		int m;
+		int arr[][]=new int[n][n];
+		int brr[][]=new int [n][n];
+		int ref[]=new int [n];
+		int ref2[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    for(int j=0;j<n;j++)
+		    {
+		        arr[i][j]=sc.nextInt();
+		    }
+		}
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<n;j++)
+	       {
+	           brr[i][j]=arr[j][i];
+	       }
+	   }
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<n;j++)
+	       {
+	           ref[j]=arr[i][j];
+	       }
+	       for(int k=0;k<n;k++)
+	       {
+	           int count=0;
+	           for(int g=0;g<n;g++)
+	           {
+	               if(ref[g]==brr[k][g])
+	               {
+	                   c++;
+	               }
+	           }
+	           if(c==n)
+	           {
+	               flag=1;
+	
+	           }
+	       }
+	       if(flag==1)
+	       {
+	           System.out.print("YES");
+	           break;
+	       }
+	   }
+	   if(flag==0)
+	   {
+	       for(int a=0;a<n;a++)
+	       {
+	           m=n-1;
+	           for(int b=0;b<n;b++)
+	           {
+	              ref[b]=arr[a][b];
+	           }
+	           for(int f=0;f<n;f++)
+	           {
+	               ref2[f]=ref[m];
+	               m--;
+	           }
+	          
+	           for(int q=0;q<n;q++)
+	           {
+	               int count=0;
+	               for(int w=0;w<n;w++)
+	               {
+	                   if(ref2[w]==brr[q][w])
+	                   {
+	                       count++;
+	                   }
+	               }
+	               if(count==n)
+	               {
+	                   flag=1;
+	               }
+	           }
+	           if(flag==1)
+	           {
+	               System.out.print("YES");
+	               break;
+	           }
+	       }
+	        if(flag==0)
+	       {
+	           System.out.print("NO");
+	       }
+	   }
+    }
+}
